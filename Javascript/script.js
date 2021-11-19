2f05225f6059ee841d04fde90c05153fa0a53747
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2021-11-18 21:27:14 -0300

    Finish

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,12 +3,12 @@ const signupBtn = document.querySelector('.signupBtn');
 const formAx = document.querySelector('.formAx');
 const body = document.querySelector('body')
 
-signupBtn.onclick = function() {
+signupBtn.onclick = function(){
     formAx.classList.add('active')
     body.classList.add('active')
 }
 
-signinBtn.onclick = function() {
+signinBtn.onclick = function(){
     formAx.classList.remove('active')
     body.classList.remove('active') 
 }
